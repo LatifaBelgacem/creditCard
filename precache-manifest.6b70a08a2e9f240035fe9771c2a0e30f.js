7c64ffb32f3f09aab35959a49471c162cbcfeb31
LatifaBelgacem <latifabelgacem320@gmail.com>
2019-08-16 11:44:28 +0100

    Updates

---
+++ precache-manifest.6b70a08a2e9f240035fe9771c2a0e30f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd834498ae7a6f2915a82449284ea48f",
+    "url": "/creditCard/index.html"
+  },
+  {
+    "revision": "856bfd69a0f04891ef93",
+    "url": "/creditCard/static/css/main.069a5332.chunk.css"
+  },
+  {
+    "revision": "4c279ffdf2322aa11c86",
+    "url": "/creditCard/static/js/2.878c0c35.chunk.js"
+  },
+  {
+    "revision": "856bfd69a0f04891ef93",
+    "url": "/creditCard/static/js/main.29e3b5ce.chunk.js"
+  },
+  {
+    "revision": "33cb2c49b6e622302f79",
+    "url": "/creditCard/static/js/runtime~main.72ca9cd9.js"
+  }
+]);
